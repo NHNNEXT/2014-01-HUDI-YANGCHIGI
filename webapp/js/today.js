8eac401270ef3ee9cfffc6f6b5cc988ffb3c12e7
minhyeok92 <minhyeok92@nhnnext.org>
2014-04-30 11:49:07 +0900

    댓글 동적 url 조정과 js fix

--- webapp/js/today.js
+++ webapp/js/today.js
@@ -1,30 +1,7 @@
-function submitComment() {
-		
-		var contentsVal = $('#commentInput').val();
-		
-		if (contentsVal == "") {
-			alert("내용이 없습니다");
-		}
-		else {
-			$.ajax({
-				type : "POST",
-				url : "/today/2/writecomment",
-				data : {
-					content : contentsVal
-				}
-			}).done(function(time) {
-				
-				$('#commentDiv').append('<div class="comment-set">'
-						+ contentsVal
-						+ '</div>').children(':last').hide().fadeIn('slow');
-				$('html, body').animate({ scrollTop: $(document).height() }, "fast");
-				$('#commentInput').val("");
-			});
-		}
-}
+
 
 function setHeightForTimeDiv() {
-	$('.timeDiv').each(
+	$('.time').each(
 			function(i) {
 				var newHeight = parseInt($(this).parent().height())
 						+ parseInt($(this).parent().css('margin-top')) * 2;
--- webapp/today.jsp
+++ webapp/today.jsp
@@ -61,6 +61,31 @@
 		</div>
 	</div>
 	<script>
+	
+function submitComment() {
+		
+		var contentsVal = $('#commentInput').val();
+		
+		if (contentsVal == "") {
+			alert("내용이 없습니다");
+		}
+		else {
+			$.ajax({
+				type : "POST",
+				url : "/today/" + ${today.id} + "/writecomment",
+				data : {
+					content : contentsVal
+				}
+			}).done(function(time) {
+				
+				$('#commentDiv').append('<div class="comment-set">'
+						+ contentsVal
+						+ '</div>').children(':last').hide().fadeIn('slow');
+				$('html, body').animate({ scrollTop: $(document).height() }, "fast");
+				$('#commentInput').val("");
+			});
+		}
+}
 		var like = {
 			likeBtn : $('#likeBtn'),
 			likeInput : $('#likeInput'),
