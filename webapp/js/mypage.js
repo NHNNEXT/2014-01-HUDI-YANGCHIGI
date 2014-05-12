bbec0a1bbcc7936a9e4acae1c4a2783e31c04f04
minhyeok92 <minhyeok92@nhnnext.org>
2014-05-12 13:41:03 +0900

    js 객체

--- webapp/js/mypage.js
+++ webapp/js/mypage.js
@@ -1,77 +1,144 @@
-var cur_img_name;
+var readImg = {
+	cur_img_name : null,
 
-function readImg() {
-	var input = $('#fileInput')[0];
-	if (input.files && input.files[0]) {
-		var reader = new FileReader();
+	read : function() {
+		var input = $('#fileInput')[0];
 
-		reader.onload = function(e) {
-			$('#prevImg').attr('src', e.target.result).height(50).show("slow");
-		};
-		cur_img_name = input.files[0].name;
-		reader.readAsDataURL(input.files[0]);
-	} else {
-		// if cancel selection
-		$('#prevImg').hide("slow");
+		if (input.files && input.files[0]) {
+			var reader = new FileReader();
+
+			reader.onload = function(e) {
+				$('#prevImg').attr('src', e.target.result).height(50).show(
+						"slow");
+			};
+			cur_img_name = input.files[0].name;
+			reader.readAsDataURL(input.files[0]);
+		} else {
+			// if cancel selection
+			$('#prevImg').hide("slow");
+		}
+	},
+
+	init : function() {
+		$('#uploadImg').click(function() {
+			$('#fileInput').click();
+		});
+		$('#fileInput').change(this.read);
 	}
 }
 
-function submitArticle() {
-	var content = $('#contentInput').val();
-	var isPrivate = $('#isPrivateIpnut:checked').val();
-	var imgName = $('#fileInput').val().split('\\')[2];
+var write = {
+	submitArticle : function() {
+		var content = $('#contentInput').val();
+		var isPrivate = $('#isPrivateIpnut:checked').val();
+		var imgName = $('#fileInput').val().split('\\')[2];
 
-	if (hasError(content, imgName))
-		return;
+		if (hasError(content, imgName))
+			return;
 
-	if (content == "") {
-		alert("내용이 없습니다");
-	} else {
-		var option = {
-			type : "POST",
-			url : "/mypage/write",
-			mimeType : "multipart/form-data",
-			data : $('body form').serialize(),
-			success : function(time) {
-				addDivAfterAjax(content, time, imgName);
+		if (content == "") {
+			alert("내용이 없습니다");
+		} else {
+			var option = {
+				type : "POST",
+				url : "/mypage/write",
+				mimeType : "multipart/form-data",
+				data : $('body form').serialize(),
+				success : function(time) {
+					this.addDivAfterAjax(content, time, imgName);
+				}.bind(this)
+			};
+			$('body form').ajaxSubmit(option);
+		}
+	},
+
+	addDivAfterAjax : function(content, time, imgName) {
+		if (imgName === undefined) {
+			$('#contentsContainerDiv').append(
+					'<div class="row contents">'
+							+ '<div class="time" ><p class="date">' + time
+							+ '</p></div>' + '<p class="contentsP">' + content
+							+ '</p></div>').children(':last').hide().fadeIn(
+					'slow');
+		} else {
+			$('#contentsContainerDiv').append(
+					'<div class="row contents">'
+							+ '<div class="time"><p class="date">' + time
+							+ '</p></div>'
+							+ '<img class="contentsImg" src="image/' + imgName
+							+ '" style="margin-right:5px;">'
+							+ '<p class="contentsP">' + content + '</p></div>')
+					.children(':last').hide().fadeIn('slow');
+		}
+
+		$('html, body').animate({
+			scrollTop : $(document).height()
+		}, "fast", this.setHeightForTimeDiv);
+		$('#prevImg').hide();
+		$('body form')[0].reset();
+		$('#textlength').text("0/200");
+	},
+
+	setHeightForTimeDiv : function() {
+		$('.time').each(
+				function(i) {
+					var newHeight = parseInt($(this).parent().height(), 10)
+							+ parseInt($(this).parent().css('margin-top'), 10)
+							* 2;
+					$(this).height(newHeight);
+				});
+	},
+	init : function() {
+		$('#submitBtn').click(this.submitArticle.bind(this));
+		this.setHeightForTimeDiv();
+		$('body').hide().delay(200).fadeIn('slow'); // check
+	}
+}
+
+var myCalendar = {
+	checkHasToday : function() {
+		$.ajax({
+			url : "/today/getList",
+			dataType : "json",
+			success : function(data) {
+				$.each($('td.fc-day'),function(i, val) {
+						$.each(data,function(j, today) {
+							if ($(this).attr('data-date') == today.date.toString()) {
+								$(this).css('background-color','rgb(88, 194, 147)');
+								$(this).click(function() {
+									window.location.href = "/today/"+ today.id;
+									});
+								}
+				}.bind(this));
+				});
 			}
-		};
-		$('body form').ajaxSubmit(option);
+		});
+	},
+
+	init : function() {
+		$('#calendarDiv').fullCalendar({
+			header : {
+				left : '',
+				center : 'title',
+				right : 'prev,next'
+			},
+
+			dayClick : function(date) {
+				// 하루를 추가해 한국 날짜로 변환
+				date.setDate(date.getDate() + 1);
+				// 오늘 날짜
+				var curDate = new Date().toISOString().substring(0, 10);
+				// 선택한 날짜
+				var clickedDate = date.toISOString().substring(0, 10);
+
+			}
+
+		});
+
+		this.checkHasToday();
+		$('.fc-button').click(this.checkHasToday);
+
 	}
-}
-
-function addDivAfterAjax(content, time, imgName) {
-	if (imgName === undefined) {
-		$('#contentsContainerDiv').append(
-				'<div class="row contents">'
-						+ '<div class="time" ><p class="date">' + time
-						+ '</p></div>' + '<p class="contentsP">' + content
-						+ '</p></div>').children(':last').hide().fadeIn('slow');
-	} else {
-		$('#contentsContainerDiv').append(
-				'<div class="row contents">'
-						+ '<div class="time"><p class="date">' + time
-						+ '</p></div>' + '<img class="contentsImg" src="image/'
-						+ imgName + '" style="margin-right:5px;">'
-						+ '<p class="contentsP">' + content + '</p></div>')
-				.children(':last').hide().fadeIn('slow');
-	}
-
-	$('html, body').animate({
-		scrollTop : $(document).height()
-	}, "fast", setHeightForTimeDiv);
-	$('#prevImg').hide();
-	$('body form')[0].reset();
-	$('#textlength').text("0/200");
-}
-
-function setHeightForTimeDiv() {
-	$('.time').each(
-			function(i) {
-				var newHeight = parseInt($(this).parent().height())
-						+ parseInt($(this).parent().css('margin-top')) * 2;
-				$(this).height(newHeight);
-			});
 }
 
 function hasError(content, imgName) {
@@ -93,64 +160,11 @@ function getLength() {
 }
 
 function load() {
-	$('#submitBtn').click(submitArticle);
-
-	$('#uploadImg').click(function() {
-		$('#fileInput').click();
-	});
-	$('#fileInput').change(readImg);
+	myCalendar.init();
+	readImg.init();
+	write.init();
 	$('#contentInput').keyup(getLength);
 
-	setHeightForTimeDiv();
-	$('body').hide().delay(200).fadeIn('slow');
-
 }
-
-var checkHasToday = function() {
-	$.ajax({
-		url : "/today/getList",
-		dataType : "json",
-		success : function(data) {
-
-			$.each($('td.fc-day'), function(i, val) {
-				$.each(data, function(j, today) {
-					if ($(this).attr('data-date') == today.date.toString()){
-						$(this).css('background-color', 'rgb(88, 194, 147)');
-						$(this).click(function(){
-							window.location.href = "/today/" + today.id;
-						});
-					}
-				}.bind(this));
-			});
-
-		}
-	});
-}
-
-// test calendar
-$(document).ready(function() {
-	$('#calendarDiv').fullCalendar({
-		header : {
-			left : '',
-			center : 'title',
-			right : 'prev,next'
-		},
-
-		dayClick : function(date) {
-			// 하루를 추가해 한국 날짜로 변환
-			date.setDate(date.getDate() + 1);
-			// 오늘 날짜
-			var curDate = new Date().toISOString().substring(0, 10);
-			// 선택한 날짜
-			var clickedDate = date.toISOString().substring(0, 10);
-
-			}
-		
-	});
-
-	checkHasToday();
-	$('.fc-button').click(checkHasToday);
-
-});
 
 window.onload = load;
