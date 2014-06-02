751dc74f9b72e30327b2b807679742910ab56b46
Jehyeok <hook3748@gmail.com>
2014-06-02 12:26:57 +0900

    #168 header.js 객체로 변환

--- webapp/js/header.js
+++ webapp/js/header.js
@@ -1,44 +1,42 @@
-// header events
-function addLogoutEvent() {
-	$('#logoutBtn').click(function() {
-		$.ajax({
-			type : "POST",
-			url : "user/logout"
-		}).done(function(msg) {
-			console.log(msg);
-			if ('success' === msg) {
-				window.location = '/';
-			}
+var Header = {
+	logoutBtn : $('#logoutBtn'),
+	showTodaysBtn : $('#showTodaysBtn'),
+	showMyPageBtn : $('#showMyPageBtn'),
+	showSettingBtn : $('#showSettingBtn'),
+	
+	addLogoutEvent : function() {
+		this.logoutBtn.click(function() {
+			$.ajax({
+				type : "POST",
+				url : "user/logout"
+			}).done(function(msg) {
+				console.log(msg);
+				if ('success' === msg) {
+					window.location = '/';
+				}
+			});
 		});
-	});
+	},
+	addShowTodaysEvent : function() {
+		this.showTodaysBtn.click(function() {
+			window.location = '/today';
+		});
+	},
+	addShowMyPageEvent : function() {
+		this.showMyPageBtn.click(function() {
+			window.location = '/';
+		});
+	},
+	addShowSettingEvent : function() {
+		this.showSettingBtn.click(function() {
+			window.location = '/usermodify';
+		});
+	},
+	init: function() {
+		this.addLogoutEvent();
+		this.addShowTodaysEvent();
+		this.addShowMyPageEvent();
+		this.addShowSettingEvent();
+	}
 }
-
-function addShowTodaysEvent() {
-	$('#showTodaysBtn').click(function() {
-		window.location = '/today';
-	});
-}
-
-function addShowMyPageEvent() {
-	$('#showMyPageBtn').click(function() {
-		window.location = '/';
-	});
-}
-function addShowSettingEvent() {
-	$('#showSettingBtn').click(function() {
-		window.location = '/usermodify';
-	});
-}
-
-addLogoutEvent();
-addShowTodaysEvent();
-addShowMyPageEvent();
-addShowSettingEvent();
-
-// 각 내용 받아오기
-/*
- * var contents = $('.contents'); $.each(contents, function(key, value) {
- * console.log('time: ' + $(value).find('.time').text()); console.log('imgName: ' +
- * $(value).find('img').attr('src')); console.log('content: ' +
- * $(value).find('.contentsP').text()); });
- */
+Header.init();
